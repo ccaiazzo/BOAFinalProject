61546b27976ed0093681d18f3900955e6fcc39a3
Marie Cari <mcari@nyc.yearup.org>
2023-01-05 21:25:44 -0500

    declare variables

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int gameBoardLength = 4; //this will be the size of the game board
+        char water = '~'; //this will be the character symbol for the ship
+        char ship = 's'; //this will be the character symbol for s
+        char hit = 'X'; //this will be the character symbol for a hit
+        char miss = 'O'; //this will be the character symbol for a miss
+        int shipNumber = 3; //this will be the number of ships
+
+    }
+}
